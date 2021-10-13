f55c87b114debaa1725bf9a236ad6a6864752360
manticorevault <artur_seilah@hotmail.com>
2021-10-13 17:04:39 -0300

    Created a small Guess List app to save users to a list using React state with TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom";
+import GuestList from "./state/GuestList";
 
 const App = () => {
     return <div>
         <h1>
-            Hi there! 
+            <GuestList />
         </h1>
     </div>
 };
